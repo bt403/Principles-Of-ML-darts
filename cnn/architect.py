9b67bb3caa78adb02b0396bacc90e2e2b529cffa
Bruno T <bruno@dhcp-10-248-117-186.eduroam.wireless.private.cam.ac.uk>
2022-01-06 00:05:46 +0000

    fix sizes, and variable types

--- cnn/architect.py
+++ cnn/architect.py
@@ -52,6 +52,8 @@ class Architect(object):
 
     unrolled_loss.backward()
     dalpha = [v.grad for v in unrolled_model.arch_parameters()]
+    print("-----")
+    print(unrolled_model.parameters())
     vector = [v.grad.data for v in unrolled_model.parameters()]
     implicit_grads = self._hessian_vector_product(vector, input_train_p, target_train_p, input_train_n, target_train_n)
 

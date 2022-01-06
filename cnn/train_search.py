572da272305df153810d456593d5ec5e188c7640
Bruno T <bruno@dhcp-10-248-123-56.eduroam.wireless.private.cam.ac.uk>
2022-01-06 18:07:55 +0000

    debug loss

--- cnn/train_search.py
+++ cnn/train_search.py
@@ -144,8 +144,8 @@ def main():
     logging.info('train_acc %f', train_acc)
 
     # validation
-    valid_acc, valid_obj = infer(valid_queue, model, criterion)
-    logging.info('valid_acc %f', valid_acc)
+    #valid_acc, valid_obj = infer(valid_queue, model, criterion)
+    #logging.info('valid_acc %f', valid_acc)
 
     utils.save(model, os.path.join(args.save, 'weights.pt'))
 

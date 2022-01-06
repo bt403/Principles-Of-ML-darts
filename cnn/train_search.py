0f0a81ffb95aff5da3803c305fe81351804cb4e0
Bruno T <bruno@dhcp-10-248-123-56.eduroam.wireless.private.cam.ac.uk>
2022-01-06 17:55:26 +0000

    fix sizes, and variable types

--- cnn/train_search.py
+++ cnn/train_search.py
@@ -159,13 +159,13 @@ def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr):
     anchor_img_search , positive_img_search , negative_img_search , anchor_label_search , negative_label_search = next(iter(train_queue))
     #input_search = Variable(input_search, requires_grad=False).cuda()
     #target_search = Variable(target_search, requires_grad=False).cuda(non_blocking=True)
-    anchor_img = Variable(anchor_img, requires_grad=True).cuda()
-    positive_img = Variable(positive_img, requires_grad=True).cuda()
-    negative_img = Variable(negative_img, requires_grad=True).cuda()
+    anchor_img = Variable(anchor_img, requires_grad=False).cuda()
+    positive_img = Variable(positive_img, requires_grad=False).cuda()
+    negative_img = Variable(negative_img, requires_grad=False).cuda()
 
-    anchor_img_search = Variable(anchor_img, requires_grad=True).cuda()
-    positive_img_search = Variable(anchor_img, requires_grad=True).cuda()
-    negative_img_search = Variable(anchor_img, requires_grad=True).cuda()
+    anchor_img_search = Variable(anchor_img, requires_grad=False).cuda()
+    positive_img_search = Variable(anchor_img, requires_grad=False).cuda()
+    negative_img_search = Variable(anchor_img, requires_grad=False).cuda()
 
     anchor_out = model(anchor_img)
     positive_out = model(positive_img)
@@ -179,8 +179,8 @@ def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr):
     labels_n_search = Variable(torch.from_numpy(np.zeros((1, negative_out_search.shape[0]), dtype=None)), requires_grad=False).cuda(non_blocking=True)
 
     #architect.step(input, target, input_search, target_search, lr, optimizer, unrolled=args.unrolled)
-    with torch.autograd.set_detect_anomaly(True):
-      architect.step(anchor_img.cuda(), positive_img.cuda(), negative_img.cuda(), labels_p, labels_n,
+    #with torch.autograd.set_detect_anomaly(True):
+    architect.step(anchor_img.cuda(), positive_img.cuda(), negative_img.cuda(), labels_p, labels_n,
       anchor_img_search.cuda(), positive_img_search.cuda(), negative_img_search.cuda(), labels_p_search, labels_n_search, lr, optimizer, unrolled=args.unrolled)
 
     optimizer.zero_grad()

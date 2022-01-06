1ac1d433743d65cf02d1600b3df1a039a6085e98
Bruno T <bruno@dhcp-10-248-117-186.eduroam.wireless.private.cam.ac.uk>
2022-01-06 00:03:13 +0000

    fix sizes, and variable types

--- cnn/train_search.py
+++ cnn/train_search.py
@@ -190,13 +190,13 @@ def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr):
 
     #architect.step(input, target, input_search, target_search, lr, optimizer, unrolled=args.unrolled)
     architect.step(dist_p.cuda(), 
-    Variable(torch.from_numpy(labels_p), requires_grad=False).cuda(), 
+    Variable(torch.from_numpy(labels_p)).cuda(), 
     dist_n.cuda(), 
-    Variable(torch.from_numpy(labels_n), requires_grad=False).cuda(), 
+    Variable(torch.from_numpy(labels_n)).cuda(), 
     dist_p_search.cuda(), 
-    Variable(torch.from_numpy(labels_p_search), requires_grad=False).cuda(), 
+    Variable(torch.from_numpy(labels_p_search)).cuda(), 
     dist_n_search.cuda(), 
-    Variable(torch.from_numpy(labels_n_search), requires_grad=False).cuda(), lr, optimizer, unrolled=args.unrolled)
+    Variable(torch.from_numpy(labels_n_search)).cuda(), lr, optimizer, unrolled=args.unrolled)
 
     loss = loss_n + loss_p
 
